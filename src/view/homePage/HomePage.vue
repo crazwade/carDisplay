a4eeba6472e1f03a414eee5ed6430652cf2d7751
crazwade <jackywu526@gmail.com>
2023-07-20 22:47:39 +0800

    feat: HomePage 分成 header / CardItem 兩個元件

--- src/view/homePage/HomePage.vue
+++ src/view/homePage/HomePage.vue
@@ -1,49 +1,54 @@
 <template>
-  <el-row :gutter="20">
-    <el-col
-      v-for="(item, index) in data"
-      :key="index"
-      :span="isCancelSpan ? 24 : 12"
-      class="py-[10px]"
-    >
-      <el-card
-        :body-style="{ padding: '0px' }"
+  <el-header>
+    header
+  </el-header>
+  <el-main>
+    <el-row :gutter="20">
+      <el-col
+        v-for="(item, index) in data"
+        :key="index"
+        :span="isCancelSpan ? 24 : 12"
+        class="py-[10px]"
       >
-        <el-carousel
-          ref="carousel"
-          arrow="always"
-          :autoplay="false"
-          :indicator-position="'none'"
+        <el-card
+          :body-style="{ padding: '0px' }"
         >
-          <el-carousel-item
-            v-for="(info, index2) in item.imgs"
-            :key="index2"
+          <el-carousel
+            ref="carousel"
+            arrow="always"
+            :autoplay="false"
+            :indicator-position="'none'"
           >
-            <img
-              :src="info.src"
-              class="image w-full h-full object-cover"
+            <el-carousel-item
+              v-for="(info, index2) in item.imgs"
+              :key="index2"
             >
-          </el-carousel-item>
-        </el-carousel>
-        <div style="padding: 14px">
-          <div>
-            <h2>{{ item.name }}</h2>
+              <img
+                :src="info.src"
+                class="image w-full h-full object-cover"
+              >
+            </el-carousel-item>
+          </el-carousel>
+          <div style="padding: 14px">
+            <div>
+              <h2>{{ item.name }}</h2>
+            </div>
+            <div class=" py-[10px]">
+              <span>{{ item.time }}</span>
+            </div>
+            <div class="bottom">
+              <el-button
+                type="info"
+                class="button"
+              >
+                詳細資訊
+              </el-button>
+            </div>
           </div>
-          <div class=" py-[10px]">
-            <span>{{ item.time }}</span>
-          </div>
-          <div class="bottom">
-            <el-button
-              type="info"
-              class="button"
-            >
-              詳細資訊
-            </el-button>
-          </div>
-        </div>
-      </el-card>
-    </el-col>
-  </el-row>
+        </el-card>
+      </el-col>
+    </el-row>
+  </el-main>
 </template>
 
 <script setup lang='ts'>
