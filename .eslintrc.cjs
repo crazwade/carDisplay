6b1ed6452f2b38c7f759741e0c6a24316eee893f
jacky <jackywu526@gmail.com>
2023-07-21 14:45:14 +0800

    feat: eslintrc 處理空格縮排以及 defineEmits 的錯誤處理˙

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,20 @@
 module.exports = {
-    env: {
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:vue/vue3-recommended',
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        parser: '@typescript-eslint/parser',
-        sourceType: 'module',
-    },
-    rules: {
-        // 句尾缺少 ;
-        semi: 'error'
-    }
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  rules: {
+    // 句尾缺少 ;
+    semi: 'error',
+    // 處理 defineEmits "is defined but never used" 問題
+    'no-unused-vars': ['error', { vars: 'all', args: 'none', ignoreRestSiblings: true }],
+  },
 };
