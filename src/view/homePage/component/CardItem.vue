d0f899d6eb09a7c3f815ec4d0d636ec79cd8495b
jacky <jackywu526@gmail.com>
2023-07-21 14:49:10 +0800

    feat: CardItem 添加如果查無資料畫面

--- src/view/homePage/component/CardItem.vue
+++ src/view/homePage/component/CardItem.vue
@@ -1,9 +1,23 @@
 <template>
   <el-row :gutter="20">
+    <div
+      v-if="data.length === 0"
+      class=" w-full"
+    >
+      <el-col :span="24">
+        <el-card
+          :body-style="{ padding: '0px' }"
+        >
+          <div style="padding: 14px">
+            查無資料...
+          </div>
+        </el-card>
+      </el-col>
+    </div>
     <el-col
       v-for="(item, index) in data"
       :key="index"
-      :span="isCancelSpan ? 24 : 12"
+      :span="isCancelSpan || data.length === 1 ? 24 : 12"
       class="py-[10px]"
     >
       <el-card
@@ -47,16 +61,14 @@
 </template>
 
 <script setup lang='ts'>
-const props = defineProps<{
+defineProps<{
   data: Array<{
     name: string,
-    time: string,
+    time: Date,
     imgs: Array<{ src: string }>,
   }>,
   isCancelSpan: boolean,
 }>();
-
-console.log(props);
 </script>
 
 <style lang="scss" scoped>
