dcbd11d49ca485413dd3e9f1912ccd9e9d8bc4b8
crazwade <jackywu526@gmail.com>
2023-07-22 03:46:18 +0800

    feat: Dialog 補上車輛資訊 Table

--- src/components/DialogView.vue
+++ src/components/DialogView.vue
@@ -49,6 +49,25 @@
           </el-carousel-item>
         </el-carousel>
       </el-col>
+      <el-col
+        :span="24"
+        class="py-2"
+      >
+        <el-table
+          style="width: 100%"
+          :data="getValues()"
+          :show-header="false"
+        >
+          <el-table-column
+            prop="title"
+            width="200"
+          />
+          <el-table-column
+            prop="value"
+            width="120"
+          />
+        </el-table>
+      </el-col>
     </el-row>
     <template #footer>
       <span class="dialog-footer">
@@ -60,14 +79,11 @@
 
 <script setup lang='ts'>
 import { ref, watch } from 'vue';
+import type { DataList } from '../view/homePage/HomePage.vue';
 
 const props = defineProps<{
   isVisible: boolean;
-  dislogData: {
-    name: string,
-    cost: string,
-    imgs: Array<{ src: string }>,
-  }
+  dislogData: DataList;
 }>();
 
 const emits = defineEmits<{
@@ -75,17 +91,65 @@ const emits = defineEmits<{
 }>();
 
 const dialogVisible = ref(false);
+const info = ref();
+
+const headers = [
+  {
+    prop: 'fuelConsumption',
+    label: '油耗',
+  },
+  {
+    prop: 'SeatMaterial',
+    label: '座椅材質',
+  },
+  {
+    prop: 'ReversingSystem',
+    label: '倒車顯影',
+  },
+  {
+    prop: 'Keyless',
+    label: 'Keyless',
+  },
+  {
+    prop: 'AutoHold',
+    label: 'Auto Hold',
+  },
+  {
+    prop: 'Digitalnstrument',
+    label: '矩陣頭燈',
+  },
+  {
+    prop: 'LaneDepartureWarning',
+    label: '道路偏移警示',
+  },
+  {
+    prop: 'ActiveSteeringHeadlights',
+    label: '主動轉向頭燈',
+  },
+  {
+    prop: 'AutomaticSwitchingLightL',
+    label: '遠近光自動切換',
+  },
+];
 
 watch(() => props.isVisible, () => {
   dialogVisible.value = props.isVisible;
+  info.value = props.dislogData.info;
+  console.log(getValues());
+  console.log(info.value);
 });
 
 const handleClose = () => {
   emits('close');
 };
 
+const getValues = () => {
+  return headers.map((item) => {
+    return [info.value].reduce((pre, cur) => Object.assign(pre, {'value': cur[item.prop]}), {'title': item.label,});
+  });
+};
+
 </script>
 
 <style lang="scss" scoped>
-
 </style>
