4bafc0e9832e5044f967b48d97a21c09e4bb3e13
crazwade <jackywu526@gmail.com>
2023-07-22 03:54:20 +0800

    feat: build dist / 新增伺服器配置

---
+++ dist/assets/ComparePage-2a9d533e.js
@@ -0,0 +1,1 @@
+import{_ as e,o,h as r}from"./index-b77f95ef.js";const a={};function c(n,t){return o(),r("div",null," ComparePage ")}const _=e(a,[["render",c]]);export{_ as default};
---
+++ dist/assets/NotFoundPage-06f8d902.js
@@ -0,0 +1,1 @@
+import{_ as e,o,h as n}from"./index-b77f95ef.js";const t={};function c(r,a){return o(),n("div",null," Not Found ")}const _=e(t,[["render",c]]);export{_ as default};
