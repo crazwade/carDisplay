b1e9000f974d22a235958f63d5c4a981a9add9f0
jacky <jackywu526@gmail.com>
2023-07-21 11:08:47 +0800

    fix: Eslint 辨識 TS 類別失敗

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,11 @@ module.exports = {
         'eslint:recommended',
         'plugin:vue/vue3-recommended',
     ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+    },
     rules: {
         // 句尾缺少 ;
         semi: 'error'
