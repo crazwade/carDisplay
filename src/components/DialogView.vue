040604388ed0656ab679a1729661ee4e3870e3b6
crazwade <jackywu526@gmail.com>
2023-07-22 01:43:12 +0800

    feat: Dialog 樣式調整

--- src/components/DialogView.vue
+++ src/components/DialogView.vue
@@ -6,22 +6,50 @@
     :before-close="handleClose"
     :lock-scroll="true"
   >
-    <span>{{ dislogData.name }}</span>
-    <el-carousel
-      :autoplay="false"
-      :indicator-position="'none'"
-    >
-      <el-carousel-item
-        v-for="(item, index) in dislogData.imgs"
-        :key="index"
+    <el-row>
+      <el-col
+        :span="24"
+        class=" py-2"
       >
-        <el-image
-          :src="item.src"
-          :preview-src-list="dislogData.imgs.map((info) => info.src)"
-          :preview-teleported="true"
-        />
-      </el-carousel-item>
-    </el-carousel>
+        <span class=" text-xl">{{ dislogData.name }}</span>
+      </el-col>
+      <el-col
+        :span="24"
+      >
+        <div class=" py-[10px] flex justify-center items-center">
+          <el-icon
+            color="#cd7f32"
+            size="24"
+          >
+            <Money />
+          </el-icon>
+          <span class=" ml-5">
+            {{ Number(dislogData.cost).toLocaleString() }}
+          </span>
+        </div>
+      </el-col>
+      <el-col
+        :span="24"
+        class=" py-2"
+      >
+        <el-carousel
+          :autoplay="false"
+          :indicator-position="'none'"
+        >
+          <el-carousel-item
+            v-for="(item, index) in dislogData.imgs"
+            :key="index"
+          >
+            <el-image
+              :src="item.src"
+              :preview-src-list="dislogData.imgs.map((info) => info.src)"
+              :preview-teleported="true"
+              class=" w-auto h-[100%] object-cover"
+            />
+          </el-carousel-item>
+        </el-carousel>
+      </el-col>
+    </el-row>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="handleClose">關閉</el-button>
